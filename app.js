84835b5a7fbff98fafb037fd06a70bc9eb5173ce
Michael Zhao <michaelzhao314@gmail.com>
2020-12-11 01:51:17 -0600

    Added embed message

--- app.js
+++ app.js
@@ -4,11 +4,35 @@ const config = require('./config.json');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const btoa = require('btoa');
 
 app.use(bodyParser.json());
 
-app.post('/', function (req, res, next) {
-    console.log(req.body);
+app.post('/', async function (req, res, next) {
+    if (req.body.action != 'opened') {
+        res.status(400);
+        res.send({ status: 400, error: 'Invalid action' });
+        return;
+    }
+    const pr = req.body.pull_request;
+    const commits = await fetch(pr.commits_url, {
+        method: 'GET',
+        headers: { Authorization: 'Basic ' + btoa(`${config.ghUser}:${config.ghAuth}`) },
+    }).then((data) => data.json());
+    const embed = new Discord.MessageEmbed()
+        .setColor('#00CD2D')
+        .setTitle(`**${pr.title}** *#${pr.number}*`)
+        .setURL(pr.html_url)
+        .setAuthor(pr.user.login, null, pr.user.html_url)
+        .setThumbnail(pr.user.avatar_url)
+        .setDescription(
+            `Merge ${pr.commits} commits into \`${pr.base.ref}\` from \`${pr.head.ref}\``
+        );
+    commits.forEach(c => {
+        embed.addField(c.sha.substring(0, 7), c.commit.message);
+    });
+    client.channels.cache.get(config.channelId).send(embed);
     res.sendStatus(200);
 });
 
